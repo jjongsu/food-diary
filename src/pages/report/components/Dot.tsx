a3da0a342c69c7bd6bff4b1f4594b4e7ff60832f
황종수 <jongsuhwang@hansol.com>
2024-11-19 14:47:10 +0900

    🔥 Feat : dot component 주석처리

---
+++ src/pages/report/components/Dot.tsx
@@ -0,0 +1,6 @@
+// import { ReactElement } from 'react';
+
+// export default function Dot(props): (props: any) => ReactElement<SVGElement> {
+//     console.log(props);
+//     return <svg></svg>;
+// }
